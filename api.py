5463e2a44e99c88f87f0faf5d7821ad1391a0d1c
Breno Coelho <breno.spinelli200@gmail.com>
2021-04-12 00:49:18 -0300

    Resposta às partes 1 e 2 do data engineering case da Lablift

---
+++ api.py
@@ -0,0 +1,40 @@
+"""
+Author: Breno Spinelli Coelho
+This code was develop to accomplish part 2 of Lablift data engineering case tasks
+
+"""
+
+from flask import Flask, jsonify
+import joblib as jb
+from part1 import patient_info
+
+app = Flask(__name__)
+
+
+@app.route('/blood', methods=['GET'])
+def first_route():
+    return 'Please, type the patient ID as a sequence for the url above', 200
+
+
+@app.route('/blood/<int:id_patient>', methods=['GET'])
+def output(id_patient):
+    observation = patient_info(id_patient)
+    if not bool(observation):
+        return 'Sorry, I could not find this patient', 404
+
+    observation['patient_id'] = id_patient
+
+    # Predicting
+    model = jb.load('blood_donation_model.joblib')
+    x1 = observation['months_since_last_donation']
+    x2 = observation['number_of_donations']
+    x3 = observation['total_volume_donated_cc']
+    x4 = observation['months_since_first_donation']
+    prediction = model.predict([[x1, x2, x3, x4]])
+    observation['prediction'] = int(prediction[0])
+
+    return jsonify(observation), 200
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ part1.py
@@ -0,0 +1,56 @@
+"""
+Author: Breno Spinelli Coelho
+This code was develop to accomplish part 1 of Lablift data engineering case tasks
+"""
+import datetime
+import pandas as pd
+
+
+# Function requested
+def patient_info(id_number):
+    """
+    This function builds a dictionary with the following keys:
+    - months_since_last_donation
+    - number_of_donations
+    - total_volume_donated_cc
+    - months_since_first_donation
+    From blood_donation_hist.csv archive
+
+    :param id_number: string that idenfies the patient
+    :return  patient_dic: dictionary with informations of the patient
+    """
+
+    # Read csv file
+    filename = 'blood_donation_hist.csv'
+    patient_dic = {}
+    data = pd.read_csv(filename)
+
+    # Current date
+    today = datetime.datetime.now()
+
+    # Find patient
+    patient_found = data['patient_id'] == id_number
+    patient = data[patient_found]
+
+    # If patient not found
+    try:
+        first_don = datetime.datetime.strptime(patient['donation_date'].values[0], '%Y-%m-%d')
+    except IndexError:
+        print("Sorry, I could not find this patient")
+        return
+
+    # Convert string from csv to datetime. Obs: 'first_donation' comes from try/except
+    last_don = datetime.datetime.strptime(patient['donation_date'].values[-1], '%Y-%m-%d')
+
+    # Write in dictionary
+    patient_dic['months_since_last_donation'] = (today.year-last_don.year)*12 + today.month - last_don.month
+    patient_dic['number_of_donations'] = len(patient['patient_id'])
+    patient_dic['total_volume_donated_cc'] = sum(patient['volume_donated_cc'])
+    patient_dic['months_since_first_donation'] = (today.year-first_don.year)*12 + today.month - first_don.month
+
+    return patient_dic
+
+
+id_number = int(input('Please, type a patient ID: '))
+print(patient_info(id_number))
+
